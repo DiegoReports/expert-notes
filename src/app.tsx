b682f5809d7a92f9a01d47553867643f2c7e570a
Diego Henrique <diego.reports@gmail.com>
2024-02-12 11:02:57 -0300

    Adjusts: Configure styles bg and cards]

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import logo from './assets/logo-nlw-expert.svg'
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12">
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -12,6 +12,44 @@ export function App() {
           className="w-full bg-transparent text-3xl font-semibold outline-none tracking-tighter placeholder: text-slate-500"
         />
       </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        {/* CARDS */}
+
+        <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative ">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto
+            automaticamente.
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-300">Há 2 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, natus
+            culpa libero eum inventore veritatis
+          </p>
+
+          <div className="absolute  bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 " />
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-300">Há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem eaque ad distinctio maxime in architecto adipisci, saepe nisi
+            eveniet tempore. Facilis, atque optio!Lorem eaque ad distinctio
+            maxime in architecto adipisci, saepe nisi eveniet tempore. Facilis,
+            atque optio! eveniet tempore. Facilis, atque optio!
+          </p>
+
+          <div className="absolute  bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 " />
+        </div>
+      </div>
     </div>
   )
 }
